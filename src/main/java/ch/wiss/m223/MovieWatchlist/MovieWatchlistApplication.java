844fd5c6572f2c2d18bcb337388c3e6a5d48011d
FloCodin <137909251+FloCodin@users.noreply.github.com>
2025-05-03 15:24:14 +0200

    Initialize Spring Boot project with Maven configuration
    Add basic project structure for MovieWatchlist, including Maven wrapper, `.gitignore`, and `.gitattributes`. Set up `application.properties` with the application name and `pom.xml` with dependencies like Spring Boot and Lombok.

---
+++ src/main/java/ch/wiss/m223/MovieWatchlist/MovieWatchlistApplication.java
@@ -0,0 +1,13 @@
+package ch.wiss.m223.MovieWatchlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieWatchlistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieWatchlistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/wiss/m223/MovieWatchlist/MovieWatchlistApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.wiss.m223.MovieWatchlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieWatchlistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
