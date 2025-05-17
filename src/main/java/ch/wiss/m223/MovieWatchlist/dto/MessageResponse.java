14a52123a0f5669439bd3ea9bf6481d4cd6a80b2
FloCodin <137909251+FloCodin@users.noreply.github.com>
2025-05-17 10:42:52 +0200

    Add MessageResponse DTO class
    Introduced a new DTO class `MessageResponse` with Lombok annotations to simplify creating response messages. This will streamline handling and returning message responses across the application.

---
+++ src/main/java/ch/wiss/m223/MovieWatchlist/dto/MessageResponse.java
@@ -0,0 +1,12 @@
+package ch.wiss.m223.MovieWatchlist.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+public class MessageResponse {
+    private String message;
+}
