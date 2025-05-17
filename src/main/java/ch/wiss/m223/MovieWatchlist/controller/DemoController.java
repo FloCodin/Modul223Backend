5253d08b4e2e5234c9c855831da932772f6f0a13
FloCodin <137909251+FloCodin@users.noreply.github.com>
2025-05-17 10:44:15 +0200

    Rename and extend DemoController with new endpoints
    The DemoController was moved to a new package and additional endpoints were added to handle private, public, and POST requests. These changes improve functionality and structure within the controller.

---
+++ src/main/java/ch/wiss/m223/MovieWatchlist/controller/DemoController.java
@@ -0,0 +1,33 @@
+package ch.wiss.m223.MovieWatchlist.controller;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class DemoController {
+
+    @GetMapping(path = "/demo")
+    public String getDemoPage(){
+        return "Hello World ;) ";
+    }
+
+    @GetMapping(path = "private")
+    public ResponseEntity<String> getTestPage() {
+        return ResponseEntity.ok()
+                .body("Protected page lalelu");
+    }
+
+    @GetMapping(path = "public")
+    public ResponseEntity<String> getPublicPage() {
+        return ResponseEntity.ok()
+                .body("Public page lalelu");
+    }
+    @PostMapping(path = "/items")
+    public ResponseEntity<String> getItems() {
+        ResponseEntity<String> result = ResponseEntity.ok()
+                .body("Admin Area");
+        return result;
+    }
+}
