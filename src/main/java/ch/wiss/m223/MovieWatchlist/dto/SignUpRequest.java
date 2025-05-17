61db3d5cf3ca8e97f2bad00b23865fa18323c791
FloCodin <137909251+FloCodin@users.noreply.github.com>
2025-05-17 10:41:39 +0200

    Add SignUpRequest DTO for user registration input handling
    Introduced a new DTO class `SignUpRequest` to handle user registration data. It includes validation annotations for fields like username, email, roles, and password to ensure proper input constraints. This will streamline and standardize the user registration process.

---
+++ src/main/java/ch/wiss/m223/MovieWatchlist/dto/SignUpRequest.java
@@ -0,0 +1,25 @@
+package ch.wiss.m223.MovieWatchlist.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Set;
+
+@Setter
+@Getter
+public class SignUpRequest {
+    @NotBlank
+    @Size(min = 3, max = 20)
+    private String username;
+    @NotBlank
+    @Size(max = 50)
+    @Email
+    private String email;
+    private Set<String> roles;
+    @NotBlank
+    @Size(min = 6, max = 40)
+    private String password;
+}
