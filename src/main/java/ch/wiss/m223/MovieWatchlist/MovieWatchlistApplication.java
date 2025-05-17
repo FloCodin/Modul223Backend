313d6066073836654626757e3bbb866fb9f95293
FloCodin <137909251+FloCodin@users.noreply.github.com>
2025-05-17 10:42:41 +0200

    Initialize default roles on application startup
    Added logic to create default roles (ROLE_USER and ROLE_ADMIN) during application startup if they do not exist. This ensures the proper setup of essential roles in the database.

--- src/main/java/ch/wiss/m223/MovieWatchlist/MovieWatchlistApplication.java
+++ src/main/java/ch/wiss/m223/MovieWatchlist/MovieWatchlistApplication.java
@@ -1,14 +1,26 @@
 package ch.wiss.m223.MovieWatchlist;
 
+import ch.wiss.m223.MovieWatchlist.repositories.RoleRepository;
+import ch.wiss.m223.MovieWatchlist.model.ERole;
+import ch.wiss.m223.MovieWatchlist.model.Role;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class MovieWatchlistApplication {
 
+@SpringBootApplication
+public class MovieWatchlistApplication implements CommandLineRunner{
+	@Autowired
+	RoleRepository roleRepository;
 	public static void main(String[] args) {
 		SpringApplication.run(MovieWatchlistApplication.class, args);
 	}
-
-
+	@Override
+	public void run(String... args) throws Exception {
+		if (roleRepository.count() == 0) {
+			roleRepository.save(new Role(ERole.ROLE_USER));
+			roleRepository.save(new Role(ERole.ROLE_ADMIN));
+		}
+	}
 }
