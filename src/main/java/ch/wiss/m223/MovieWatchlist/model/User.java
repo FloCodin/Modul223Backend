eabbed14991f920d0c491221056434f3070b88a6
FloCodin <137909251+FloCodin@users.noreply.github.com>
2025-05-27 13:57:30 +0200

    Change User ID type from Integer to Long
    Updated the ID field in the User model to use Long instead of Integer. This change ensures compatibility with larger identifier values and aligns with best practices for entity IDs.

--- src/main/java/ch/wiss/m223/MovieWatchlist/model/User.java
+++ src/main/java/ch/wiss/m223/MovieWatchlist/model/User.java
@@ -20,7 +20,7 @@ import java.util.Set;
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+    private Long id;
     @NotBlank
     private String username;
     @NotBlank
