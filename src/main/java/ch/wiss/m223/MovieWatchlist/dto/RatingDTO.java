e779433bf53a82ed77f3baddf283f6abafc6d450
FloCodin <137909251+FloCodin@users.noreply.github.com>
2025-05-27 14:01:14 +0200

    Add RatingDTO class to represent movie ratings
    Introduced a new DTO class `RatingDTO` with fields for movie details such as ID, score, title, description, and additional metadata. This establishes a foundation for handling movie ratings in the application.

---
+++ src/main/java/ch/wiss/m223/MovieWatchlist/dto/RatingDTO.java
@@ -0,0 +1,13 @@
+package ch.wiss.m223.MovieWatchlist.dto;
+
+public class RatingDTO {
+    public Long movieId;
+    public int score;
+    public String comment;
+
+    public String title;
+    public String description;
+    public String director;
+    public int duration;
+    public int year;
+}
